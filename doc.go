3f4e47c27374c1a083c45763a8e6d267ac0ebf35
Pavel Tsayukov <p.tsayukov@gmail.com>
2026-03-07 20:24:19 +0300

    📝 Add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// This file is licensed under the terms of the MIT License (see LICENSE file)
+// Copyright (c) 2026 Pavel Tsayukov p.tsayukov@gmail.com
+
+// Package cmdlog provides functions for creating development and production loggers,
+// as well as flags for command-line tools.
+package cmdlog
